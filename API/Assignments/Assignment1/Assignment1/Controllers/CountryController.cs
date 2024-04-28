d0ed4da0a6577a9c6c72e3bf7a4917682d7b8718
Avinash Singh <avinashs@infinite.com>
2024-04-28 23:56:59 +0530

    Ass1

---
+++ API/Assignments/Assignment1/Assignment1/Controllers/CountryController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Assignment1.Models;   // Adding models
+
+namespace Assignment1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        static List<Country> countrylist = new List<Country>()
+        {
+            new Country{ID=1,CountryName="India",Capital="New Delhi"},
+            new Country{ID=2,CountryName="France",Capital="Paris"},
+            new Country{ID=3,CountryName="UK",Capital="London"},
+        };
+
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Country> Get()
+        {
+            return countrylist;
+        }
+        [HttpGet]            // Read
+        [Route("Bymessage")]
+        public HttpResponseMessage GetAllCountry()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, countrylist);
+            return response;
+        }
+
+        [HttpPost]      // create..
+        [Route("AllPost")]
+        public List<Country> PostAll([FromBody] Country country)
+        {
+            countrylist.Add(country);
+            return countrylist;
+        }
+        [HttpPost]
+        [Route("countrypost")]
+        public void CountryPost([FromUri] int Id,string countryname, string captial)
+        {
+            Country country = new Country();
+            country.ID = Id;
+            country.CountryName = countryname;
+            country.Capital = captial;
+            countrylist.Add(country);
+        }
+
+
+
+        [HttpPut]
+        public void Put(int cid,[FromUri] Country c)
+        {
+            countrylist[cid - 1] = c;
+        }
+
+        [HttpDelete]
+        public void Delete(int cid)
+        {
+            countrylist.RemoveAt(cid - 1);
+        }
+    }
+}
---
+++ API/Assignments/Assignment1/Assignment1/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class Country
+    {
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+
+    }
+}
+
+
+
